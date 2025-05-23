4a42382adf0deccfb6898c85610165c63c2f8dd4
rahulsadhwani24 <rahulsadhwani24@gmail.com>
2025-05-23 21:22:34 +0530

    Save Changes

--- src/assets/Data/Projects.js
+++ src/assets/Data/Projects.js
@@ -1,26 +1,32 @@
+import Themepark from '../Theme Park.webp';
+import MarketingArc from '../Marketing Arcc.webp';
+import MaxMedical from '../Max Medical.webp';
+import TwoGoodCo from '../Two Good co.webp';
+
+
 export default [
     {
       name: "Theme Park",
       type: "Javascript",
       link: "https://rahulsadhwani24.github.io/Themepark/",
-      logo: "ThemePark.gif",
+      logo: Themepark,
     },
     {
       name: "Marketing Arcc",
       type: "Javascript",
       link: "https://rahulsadhwani24.github.io/MarketingArc/",
-      logo: "MarketingArcc.gif",
+      logo: MarketingArc,
     },
     {
       name: "Max Medical",
       type: "Javascript",
       link: "https://rahulsadhwani24.github.io/MaxMedical/",
-      logo: "MaxMedical.gif",
+      logo: MaxMedical,
     },
     {
       name: "Two Good Co.",
       type: "Javascript",
       link: "https://rahulsadhwani24.github.io/TwoGoodCo/",
-      logo: "TwoGoodCo",
+      logo: TwoGoodCo,
     },
   ]
--- src/components/PageExperiments.jsx
+++ src/components/PageExperiments.jsx
@@ -1,13 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import experiments from "../assets/Data/Experiments";
 import Loader from "./Loader";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Link } from "react-router-dom";
+gsap.registerPlugin(ScrollTrigger);
 
 function PageExperiments() {
   const [timeout, setTimeout] = useState(true);
+  const experimentContainerRef = useRef(null);
+  useEffect(() => {
+    if (!timeout && experimentContainerRef.current) {
+      const experiments = gsap.utils.toArray(
+        experimentContainerRef.current.querySelectorAll(".experiment")
+      );
+
+      gsap.to(experiments, {
+        xPercent: -100 * (experiments.length - 1.7),
+        scrollTrigger: {
+          trigger: experimentContainerRef.current,
+          pin: true,
+          scrub: 2,
+          start: "50% 50%",
+          end: () => "+=" + experimentContainerRef.current.offsetWidth,
+        },
+      });
+      return () => {
+        ScrollTrigger.getAll().forEach((trigger) => {
+          trigger.kill();
+        });
+      };
+    }
+  }, [timeout]);
   return (
     <main className={`${!timeout && "loadMain"}`}>
       {!timeout ? (
         <div className="pageExperiments">
           <h1 className="experimentsHeading">Experiments</h1>
+          <div className="experiments" ref={experimentContainerRef}>
+            {experiments.map((currExperiment, index) => {
+              return (
+                <Link className="experiment" key={index} to={currExperiment.link} target="_blank">
+                  <div>
+                    <img className="experimentImage" src={currExperiment.image} alt={currExperiment.name} />
+                    <span className="experimentName">{currExperiment.name}</span>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
       ) : (
         <Loader setTimeout={setTimeout} />
